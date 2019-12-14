5128386c65f73eb226922febd14323926cc047b8
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-14 04:05:01 +0200

    Update sl25.ts

--- sl25.ts
+++ sl25.ts
@@ -1,1 +1,50 @@
-//
+//% color=#444444 icon="\uf0eb"
+namespace SL26 {
+
+    export enum DistanceMode {
+        //% block="short"
+        Short = 0,
+        //% block="medium"
+        Medium = 1,
+        //% block="long"
+        Long = 2,
+        //% block="unknown"
+        Unknown = 3
+    }
+
+    //%block="SL25 initialize"
+    //%shim=SL25::init
+    export function init(): void {
+        return;
+    }
+
+    //%block="SL25 setDistanceMode %u"
+    //%shim=SL25::setDistanceMode
+    export function setDistanceMode(u: DistanceMode): number {
+        return 1;
+    }
+
+    //%block="SL25 setMeasurementTimingBudget"
+    //%shim=SL25::setMeasurementTimingBudget
+    export function setMeasurementTimingBudget(timing: number): void {
+        return;
+    }
+
+    //%block="SL25 startContinuous"
+    //%shim=SL25::startContinuous
+    export function startContinuous(x: number): void {
+        return;
+    }
+
+    //%block="SL25 disable power"
+    //%shim=SL25::disablePower
+    export function disbalePower(): void {
+        return;
+    }
+
+    //%block="SL25 read"
+    //%shim=SL25::read
+    export function read(): void {
+        return;
+    }
+}
